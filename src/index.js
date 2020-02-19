d4aea0f62999cb7e0aff0d423ba791048051431a
Jean-Christophe Taveau <jeanchristophe.taveau@gmail.com>
2020-02-19 11:16:27 +0100

    Add config

---
+++ src/index.js
@@ -0,0 +1,40 @@
+/*
+ *  tij: Tiny ImageJ API in JavaScript
+ *  Copyright (C) 2017-2020  Jean-Christophe Taveau.
+ *
+ *  This file is part of tij
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with tij.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Authors:
+ * Jean-Christophe Taveau
+ */
+ 
+'use strict';
+
+import {ImagePlus} from './ImagePlus';
+import {Roi} from './gui/Roi';
+import {ResultsTable} from './measure/ResultsTable';
+import {ByteProcessor} from './process/ByteProcessor';
+import {ImageProcessor} from './process/ImageProcessor';
+ 
+ 
+export {
+  ImagePlus,
+  ImageProcessor,
+  ResultsTable,
+  Roi
+}
+ 
