cb3af6f59c0375344a4d34f59cd51f8049791315
emilie desquerre <emilie.desquerre@gmail.com>
2020-04-16 23:22:32 +0200

    new tests

---
+++ Interface/test_lire.js
@@ -0,0 +1,10 @@
+function load_csv(event) {
+    let file = event.target.files(0);
+    fetch();
+    then(response);
+    let text = response.text();
+    parseCSV(text);
+}
+
+let button = document.querySelector('#script');
+button.addEventListener('file', load_csv);
---
+++ Interface/test_table.js
@@ -0,0 +1,13 @@
+function ajouterLigne(titre,nom)
+{
+	var tableau = document.getElementById("tableau");
+
+	var ligne = tableau.insertRow(-1);//on a ajouté une ligne
+
+	var colonne1 = ligne.insertCell(0);//on a une ajouté une cellule
+	colonne1.innerHTML += titre;//on y met le contenu de titre
+
+	var colonne2 = ligne.insertCell(1);//on ajoute la seconde cellule
+	colonne2.innerHTML += nom;
+}
+ajouterLigne('hello','world');
