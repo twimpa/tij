0ab97ad43e81ea66fbd5a084166af0c3ac503b64
Jean-Christophe Taveau <jeanchristophe.taveau@gmail.com>
2020-04-20 14:43:51 +0200

    Clean

---
+++ doc/examples/table001.js
@@ -0,0 +1,33 @@
+/*
+ *  tij: Tiny ImageJ API in JavaScript
+ *  Copyright (C) 2017-2020  Jean-Christophe Taveau.
+ *
+ *  This file is part of tij
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with tij.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Authors:
+ * Jean-Christophe Taveau
+ */
+
+'use strict';
+
+
+let table = new ResultsTable(10);
+table.addValue('Area',100);
+table.addValue('Area',101);
+table.addValue('Feret',200.54);
+table.show('Results001');
+
