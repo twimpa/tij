f4437138548e2a3ad9a55f6294d75b6528d4c311
tuvu960527 <56268630+tuvu960527@users.noreply.github.com>
2020-04-23 13:32:43 +0200

    Update ResultsTable.js

--- src/measure/ResultsTable.js
+++ src/measure/ResultsTable.js
@@ -1175,7 +1175,8 @@ export class ResultsTable {
    * @author Created by ijdoc2js
    */
   renameColumn(oldName, newName) {
-    throw "Not Implemented - ResultsTable.renameColumn(..)";
+    let index_oldName = this.getColumnIndex(oldName);
+    this.headings[index_oldName]=newName;
   };
 
   /**
