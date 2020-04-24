45711a8eb6ae45e4a278b644446a7f025dfc3d8c
Jean-Christophe Taveau <jeanchristophe.taveau@gmail.com>
2020-04-24 16:00:43 +0200

    First Release

---
+++ tests/ResultsTable_tij.js
@@ -0,0 +1,42 @@
+/*
+ *  tij: Tiny ImageJ API
+ *  Copyright (C) 2020  Jean-Christophe Taveau.
+ *
+ *  This file is part of tij
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with tij.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * Authors:
+ * Jean-Christophe Taveau
+ * 
+ */
+
+
+'use strict';
+
+
+describe('ResultsTable - specific to TIJ', function () {
+  describe('open(path) => int', function () {
+    it('should return a ResultsTable', function () {
+     // TODO
+    });
+  });
+  
+
+
+}); // End of tests for ResultsTable specific to TIJ
+
+
+
